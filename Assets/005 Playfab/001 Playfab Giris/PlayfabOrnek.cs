7102df7259ca6b7b99940eedb11b410432cba811
Usi133 <yusuf_sener_97@hotmail.com>
2022-04-11 08:45:34 +0300

    0

---
+++ Assets/005 Playfab/001 Playfab Giris/PlayfabOrnek.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayfabOrnek : MonoBehaviour
+{
+   /*
+    
+    Oyuncu basarisini izleyerek karsilastirarak ve odullendirerek oyunumuza daha fazla katilimi saglayan bir API destegidir. Playfab ile onlline oyunlar olusturabilir ve oyun icerisindeki bircok veriyi Microsoft Azure sunucularinda saklayabiliriz. 
+    */
+}
---
+++ Assets/005 Playfab/API/APIOrnek.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APIOrnek : MonoBehaviour
+{
+    /*
+
+    Amazon-Google(Firebase)-Microsoft Azure[We use]
+
+
+
+    API
+
+
+    Bir iletisim agidir. Kendi iclerinde get-set dondururler (*Tek bi API uzerinden get-set yapamayiz).  
+
+
+
+
+     */
+}
